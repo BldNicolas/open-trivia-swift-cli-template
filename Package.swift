e435cd78cf00a6a9f91109f452a5c1a54a3887fe
Nicolas <ni.berlaud@gmail.com>
2024-12-07 19:39:41 +0100

    💚

--- Package.swift
+++ Package.swift
@@ -1,14 +1,16 @@
 // swift-tools-version: 5.5
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "opentrivia",
+    platforms: [
+        .macOS(.v10_15) // Specify macOS 10.15 or newer
+    ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .executableTarget(
-            name: "opentrivia"),
+            name: "opentrivia",
+            path: "Sources/opentrivia"
+        )
     ]
 )
